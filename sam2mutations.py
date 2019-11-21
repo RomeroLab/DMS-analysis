59d92062cf3efc48f7699bc1072465f0bd714bdf
hridindu-dm19 <50841577+hridindu-dm19@users.noreply.github.com>
2019-11-21 12:02:31 -0600

    modified to also output pkls

--- sam2mutations.py
+++ sam2mutations.py
@@ -1,8 +1,9 @@
 import sequence_tools
 import NGS_tools
-from sys import argv
 import os
+import pickle
 
+### input files and options ##########################################
 ### generate list of .sam files ###
 samfiles = []
 for file in os.listdir():
@@ -14,14 +15,20 @@ for file in os.listdir():
         ref_fasta = file
         
 start, stop, ref_seq = find_max_orf(ref_fasta)# the start and stop indices for the coding region.  Slicing reference[start:stop] should start on start codon and end on stop codon
+coding = range(start,stop)
 
 ### load sequences and genetic code #################################
 reference = sequence_tools.read_fasta(ref_fasta)[1][0]
 WTcodons = sequence_tools.split_codons(reference[start:stop])
 
+AAs = tuple(set(sequence_tools.code.values()))
 code = sequence_tools.code
 degcode = dict((p1+p2+'N',code[p1+p2+'A']) for p1 in 'ACGT' for p2 in 'ACGT' if len(set([code[p1+p2+p3] for p3 in 'ACGT']))==1) # these are the 8 AAs where the third position doen't matter
 code.update(degcode)
+
+CDN_count = dict((i,dict((a,0) for a in code)) for i in range(len(coding)//3)) # dict of dicts
+AA_count = dict((i,dict((a,0) for a in AAs)) for i in range(len(coding)//3)) # dict of dicts
+pair_count = dict(((i,j),dict()) for i in range(len(coding)//3) for j in range(len(coding)//3) if i<j)
 
 Qcut = 30 # Quality score cutoff (QS >= Qcut is considered good)
 CDN_muts = False # return codon mutations (rather than AA mutations)
@@ -32,6 +39,7 @@ WT_reads = False # return reads that contain no mutations (necessary for countin
 def sam_to_mutations(samfile):
     output = ''
     outfile = samfile.split('/')[-1].replace('.sam','_mutations.txt')
+    pkl_outfile = samfile.split('/')[-1].replace('.sam','.pkl')
     samfile = open(samfile)
     read_count_pos = [0]*len(WTcodons) 
     mut_count_pos = [0]*len(WTcodons) 
@@ -103,7 +111,6 @@ def sam_to_mutations(samfile):
     
     open(outfile,'w').write(output)
     
-    
     # generate read counts file that contains WT sequence, read counts, and mutation counts per position 
     output = ''
     for i in range(len(WTcodons)):
@@ -111,6 +118,23 @@ def sam_to_mutations(samfile):
     
     open(outfile.replace('mutations','read_counts'),'w').write(output)
     print('Analyzed %i reads and wrote %i mutated reads to file: %s' % (sc,lc,outfile))
+    
+    #### generate pickle files with the old RE style data
+    
+    pkl_outfile = samfile.split('/')[-1].replace('.sam','.pkl')
+    codons = [i for i in enumerate(codons) if i[1] in code and (i[0]>=matches[0] and i[0]<=matches[-1])] # use codons that code for a specific amino acid and are within the valid range
+    for i in codons:
+        CDN_count[i[0]][i[1]]+=1
+        AA_count[i[0]][code[i[1]]]+=1
+    pickle.dump((CDN_count,AA_count,pair_count),open(pkl_outfile,'wb'))
 ##### loop over all sam files in directory #####
+
 for samfile in samfiles:
     sam_to_mutations(samfile)
+    
+    
+    
+    
+    
+    
+    
